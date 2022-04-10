b40aab7b32c97023d5d3601633ccc1c08d6450b8
Natalie Gil <natalieg@outlook.com>
2022-04-10 13:16:35 -0500

    Update HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,1 +1,16 @@
+//Set compiler version for Solidity
+pragma solidity ^0.8.13;
 
+
+contract HelloWorldZKU {
+    // variable to store the unsigned integer 
+    uint256 private number;
+    // store the unsigned integer
+    function storeUNIT(uint256 _number) public {
+        number = _number;
+    }
+    // retrieve the unsigned integer
+    function retrieveUINT() view public returns(uint256){
+        return number;
+    }
+}
